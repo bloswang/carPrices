fdcea2cf0ff3f96bfa0150bc27b4dafb5a30fff6
tangguotong <tangguotong@163.com>
2018-01-18 17:47:05 +0800

    ..

---
+++ carPrices/js/public.js
@@ -0,0 +1,11 @@
+$(function () {
+  /*头部的选项点击事件*/
+  $('.contents-list .item .item-content').on('click',function () {
+    $(this).addClass('blue-text').siblings().removeClass('blue-text')
+  })
+
+  /*分页的点击事件*/
+  $('.page-content').on('click','ul li',function () {
+      $(this).addClass('active').siblings().removeClass('active');
+  })
+})
